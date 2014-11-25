beeadf43023462ed2060c64659b2e899be2d70b0
Devin Mason <devinmsn@yahoo.com>
2014-11-25 02:45:12 -0800

    Add stuff

---
+++ Assets/DroppingRock.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DroppingRock : MonoBehaviour {
+
+
+	public GameObject rock;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		print ("collision");
+		rock.GetComponent<Fall> ().RockFall ();
+	}
+}
---
+++ Assets/Fall.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fall : MonoBehaviour {
+
+	public float gravityTimer = 2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gravityTimer -= Time.deltaTime;
+	}
+
+	public void RockFall()
+	{
+		if(gravityTimer <= 0)
+		{
+			transform.Translate (Vector3.down);
+		}
+	}
+}
